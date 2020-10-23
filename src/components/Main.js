1c30338b5d933f546f85b9b368477d4ec75f28f2
xternet <majewski.post@gmail.com>
2020-10-24 01:28:22 +0200

    .

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,41 +21,41 @@ render() {
 				</div>
 				<div className="col-md-2 border border-danger overflow-auto text-center" style={{ maxHeight: '768px', minWidth: '175px' }}>
 		    	<h5><b>Share Video</b></h5>
-	        <form onSubmit={(event) => {
-		          event.preventDefault()
-		          const title = this.videoTitle.value
-		          this.props.uploadVideo(title)
-	        	}} >
-	        	&nbsp;
-	          <input type='file' accept=".mp4, .mkv .ogg .wmv" onChange={this.props.captureFile} style={{ width: '250px' }} />
-	            <div className="form-group mr-sm-2">
-                <input
-                  id="videoTitle"
-                  type="text"
-                  ref={(input) => { this.videoTitle = input }}
-                  className="form-control-sm"
-                  placeholder="Title..."
-                  required />
-	            </div>
-	          <button type="submit" className="btn btn-danger btn-block btn-sm">Upload!</button>
-	          &nbsp;
-	        </form>
-						{ this.props.videos.map((video, key) => {
-		          return(
-		            <div className="card mb-4 text-center bg-secondary mx-auto" style={{ width: '175px'}} key={key} >
-		              <div className="card-title bg-dark">
-		                <small className="text-white"><b>{video.title}</b></small>
-		              </div>
-		              <div>
-			              <p onClick={() => this.props.changeVideo(video.hash, video.title)}>
-			                <video
-			                	src={`https://ipfs.infura.io/ipfs/${video.hash}`}
-			                	style={{ width: '150px' }}
-			                />
-			               </p>
-		              </div>
+		        <form onSubmit={(event) => {
+			          event.preventDefault()
+			          const title = this.videoTitle.value
+			          this.props.uploadVideo(title)
+		        	}} >
+		        	&nbsp;
+		          <input type='file' accept=".mp4, .mkv .ogg .wmv" onChange={this.props.captureFile} style={{ width: '250px' }} />
+		            <div className="form-group mr-sm-2">
+	                <input
+	                  id="videoTitle"
+	                  type="text"
+	                  ref={(input) => { this.videoTitle = input }}
+	                  className="form-control-sm"
+	                  placeholder="Title..."
+	                  required />
 		            </div>
-		          )
+		          <button type="submit" className="btn btn-danger btn-block btn-sm">Upload!</button>
+		          &nbsp;
+		        </form>
+						{ this.props.videos.map((video, key) => {
+							return(
+								<div className="card mb-4 text-center bg-secondary mx-auto" style={{ width: '175px'}} key={key} >
+									<div className="card-title bg-dark">
+										<small className="text-white"><b>{video.title}</b></small>
+									</div>
+										<div>
+											<p onClick={() => this.props.changeVideo(video.hash, video.title)}>
+												<video
+													src={`https://ipfs.infura.io/ipfs/${video.hash}`}
+													style={{ width: '150px' }}
+												/>
+											</p>
+									</div>
+								</div>
+							)
 						})}
         </div>
 			</div>
